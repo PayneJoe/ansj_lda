7797dd0e208d741fdf0722140448bfba13edf34f
PayneJoe <joepayne@163.com>
2015-12-10 14:56:49 +0800

    "update Test3.java"

--- src/main/java/org/ansj/tm/Test3.java
+++ src/main/java/org/ansj/tm/Test3.java
@@ -3,6 +3,8 @@ package org.ansj.tm;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 import org.ansj.lda.LDA;
 import org.ansj.lda.impl.LDAGibbsModel;
@@ -16,10 +18,16 @@ public class Test3 {
 	public static void main(String[] args) throws IOException {
 		LDA lda = new LDA(AnsjAnalysis.DEFAUlT,new LDAGibbsModel(40, 50/(double)40, 0.02, 150, Integer.MAX_VALUE, Integer.MAX_VALUE));
 //		String inputPath = "E:\\work\\project\\site-analysis\\ContentRetrieve\\video\\baike";
-		String inputPath = "/home/yuanpingzhou/data/ansj_lda/input/baike";
+		String inputPath1 = "/home/yuanpingzhou/data/ansj_lda/input/autotopicpost";
+		String inputPath2 = "/home/yuanpingzhou/data/ansj_lda/input/autobrandpost";
 		String outputPath = "/home/yuanpingzhou/data/ansj_lda/output/baike";
-		File file = new File(inputPath);
-		File[] inputFiles = file.listFiles();
+		File file1 = new File(inputPath1);
+		File[] inputFiles1 = file1.listFiles();
+		File file2 = new File(inputPath2);
+		File[] inputFiles2 = file2.listFiles();
+		File[] totalFiles =  new File[inputFiles1.length + inputFiles2.length];
+		Arrays.copyOf(original, newLength)
+		
 		int j = 0;
 		for(File f : inputFiles){
 			if(j == 40){
